7c07f43ffdcd718e215a57f5d270f5f3678612f5
Bruno Dahlem <bruno.dahlem2@gmail.com>
2022-04-12 13:38:36 +0200

    Added btn in tarif

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,9 @@
-// Import fast-glob package
-const fg = require('fast-glob');
-
-// Executer la recherche d'images dans /uplaods
-const galleryImages = fg.sync(['**/uploads/*.jpg', '!**/public']);
-
 module.exports = function (eleventyConfig) {
 
-    // Créer une collection d'images
-    eleventyConfig.addCollection('uploads', function(collection) {
-      return galleryImages;
-    });
-    
     // Ce dossier est cloné lors du build
     eleventyConfig.addPassthroughCopy("./src/uploads");
     eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addPassthroughCopy("./src/script");
+    eleventyConfig.addPassthroughCopy("./src/script");
   
     return { 
       dir: {
