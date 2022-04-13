6adb590ede26f47364c40cab7739a92b9826f5f9
Bruno Dahlem <bruno.dahlem2@gmail.com>
2022-04-13 08:05:24 +0200

    Added banner-nav

---
+++ src/script/main.js
@@ -0,0 +1,5 @@
+// Fetch all the filter buttons
+let btnFilter = document.querySelectorAll('.filter');
+
+// Fetch all the pictures in the gallery
+let imgGalleries = document.querySelectorAll('.img-gallery');
